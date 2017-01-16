ea098b1f5ee9cdae101c3138fc135e3f61452f5e
Ivan Prymachok <ivanprymachok@gmail.com>
2017-01-16 21:33:42 +0200

    add_empty_group test added

--- test_add_group.py
+++ test_add_group.py
@@ -61,6 +61,16 @@ class test_add_group(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
